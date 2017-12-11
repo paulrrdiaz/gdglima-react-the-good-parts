046e5a5da4de82bd0069fb0876c569a0751e6d76
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2017-12-10 22:59:21 -0500

    Create README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# GDG Lima 2017 | React - the good parts
+
+Si aún no tienes muchas experiencia con React, sería buena idea que desarrolles algunos ejemplos básicos para tener una idea más clara de los problemas que te ayudará a resolver y si es la mejor opción para tu necesidad.
+- [Documentación oficial](https://reactjs.org/)
+- [Tutorial básico de React en español](https://frontendlabs.io/3158--react-js-espanol-tutorial-basico-primeros-pasos-ejemplos)
+
+Con esos conceptos básicos podemos continuar...
--- presentation/presentation/index.js
+++ presentation/presentation/index.js
@@ -3,14 +3,9 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Code,
-  Cite,
   Deck,
   Heading,
-  ListItem,
   Link,
-  Quote,
   Slide,
   Image,
   Text
@@ -25,35 +20,33 @@ require("normalize.css");
 const theme = createTheme({
   primary: "white",
   white: "#ffffff",
-  secondary: "#1F2022",
   skyblue: "#2980b9",
-  quartenary: "#CECECE",
-  blue: "#2c3e50",
+  blue: "#2c3e50"
 }, {
   primary: "Montserrat",
   secondary: "Helvetica"
 });
 
 const images = {
-  me: require('../assets/me.jpg'),
-  react: require('../assets/react.png'),
-  frontendpe: require('../assets/frontendpe.png'),
-  things: require('../assets/things.jpg'),
-  impDec: require('../assets/imp-dec.png'),
-  impDecReact: require('../assets/imp-dec-react.png'),
-  templating: require('../assets/templating.png'),
-  multiLine: require('../assets/multi-line.png'),
-  multiProperties: require('../assets/multi-properties.png'),
-  cond1: require('../assets/cond1.png'),
-  cond2: require('../assets/cond2.png'),
-  cond3: require('../assets/cond3.png'),
-  cond4: require('../assets/cond4.png'),
-  loops: require('../assets/loops.png'),
-  control1: require('../assets/control1.png'),
-  control2: require('../assets/control2.png'),
-  control3: require('../assets/control3.png'),
-  subrender: require('../assets/subrender.png'),
-  components: require('../assets/components.png'),
+  me: require("../assets/me.jpg"),
+  react: require("../assets/react.png"),
+  frontendpe: require("../assets/frontendpe.png"),
+  things: require("../assets/things.jpg"),
+  impDec: require("../assets/imp-dec.png"),
+  impDecReact: require("../assets/imp-dec-react.png"),
+  templating: require("../assets/templating.png"),
+  multiLine: require("../assets/multi-line.png"),
+  multiProperties: require("../assets/multi-properties.png"),
+  cond1: require("../assets/cond1.png"),
+  cond2: require("../assets/cond2.png"),
+  cond3: require("../assets/cond3.png"),
+  cond4: require("../assets/cond4.png"),
+  loops: require("../assets/loops.png"),
+  control1: require("../assets/control1.png"),
+  control2: require("../assets/control2.png"),
+  control3: require("../assets/control3.png"),
+  subrender: require("../assets/subrender.png"),
+  components: require("../assets/components.png")
 };
 
 export default class Presentation extends React.Component {
@@ -61,9 +54,13 @@ export default class Presentation extends React.Component {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="blue">
-          <Image src={images.me} width="300" style={{'border-radius': '50%', border: '5px solid #fff'}} />
+          <Image src={images.react} width="300"/>
+          <Heading size={1} fit caps lineHeight={1} textColor="white">React - The Good Parts</Heading>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="blue">
+          <Image src={images.me} width="300" style={{ "border-radius": "50%", border: "5px solid #fff" }} />
           <Text margin="10px 0 0" textColor="white" size={4} bold>
-            Hi, I'm @paulrrdiaz
+            @paulrrdiaz
           </Text>
           <Text margin="30px 0 0" size={6}>
             <Link textColor="skyblue" href="https://twitter.com/paulrrdiaz" target="_blank">Twitter</Link>
@@ -71,16 +68,10 @@ export default class Presentation extends React.Component {
             <Link textColor="skyblue" href="https://www.linkedin.com/in/paulrrdiaz/" target="_blank">Linkedin</Link>
           </Text>
         </Slide>
-        <Slide transition={["zoom"]} bgColor="blue">
-          <Image src={images.react} width="300"/>
-          <Heading size={1} fit caps lineHeight={1} textColor="white">
-            React - The Good Parts
-          </Heading>
-        </Slide>
         <Slide transition={["fade"]} bgColor="blue">
           <Image src={images.frontendpe} width="150"/>
           <Heading margin="20px 0 0" size={3} lineHeight={1} textColor="white">Frontendpe</Heading>
-          <Link margin="10px 0 0" size={6} textColor="white" href="https://www.facebook.com/groups/frontendpe/" target="_blank">Click me</Link>
+          <Link margin="10px 0 0" size={6} textColor="white" href="https://www.facebook.com/groups/frontendpe/" target="_blank">Click me -please-</Link>
         </Slide>
         <Slide transition={["fade"]} bgColor="blue">
           <Image src={images.things} width="600"/>
@@ -151,6 +142,7 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide transition={["fade"]} bgColor="skyblue">
           <Heading size={3} textColor="white">Storybook</Heading>
+          <Link margin="10px 0 0" size={6} textColor="white" href="https://github.com/storybooks/storybook" target="_blank">Click me -please-</Link>
         </Slide>
         <Slide transition={["fade"]} bgColor="skyblue">
           <Heading size={3} textColor="white">Container and Presentational pattern</Heading>
@@ -163,6 +155,10 @@ export default class Presentation extends React.Component {
           <Text size={6} textColor="blue">- Render Elements outside the current Root</Text>
           <Text size={6} textColor="blue">- Define DOM attributes</Text>
           <Text size={6} textColor="blue">- Call setState with null to avoid triggering an update</Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="skyblue">
+          <Heading size={3} textColor="white">Thank you!</Heading>
+          <Text size={6} textColor="blue">if you can share this, it'd be awesome :)</Text>
         </Slide>
       </Deck>
     );
